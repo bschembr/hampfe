9aa0f9ab9dbe552c44ed68664fd96af4b963b23a
Brian Schembri <bschembri@attardco.com>
2018-10-29 13:05:33 +0100

    Added skeleton menumain component

---
+++ src/app/menu/menumain/menumain.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menumain',
+  templateUrl: './menumain.component.html',
+  styleUrls: ['./menumain.component.css']
+})
+export class MenumainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
