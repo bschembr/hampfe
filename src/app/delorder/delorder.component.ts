c26eee810a05fe9e20157854afd1af0d56cb5f07
Alvin Mallia <amallia@attardco.com>
2018-11-16 09:32:00 +0100

    Changed MatTableSummary Columns

--- src/app/delorder/delorder.component.ts
+++ src/app/delorder/delorder.component.ts
@@ -43,7 +43,7 @@ export class DelorderComponent implements OnInit, AfterViewInit, DataSource {
   filteredClients: Observable<string[]>;
 
   listHamperData: MatTableDataSource<DelNote>;
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns = ['Hamper Code', 'Invoice Qty', 'Delivery Note Qty'];
   dataSource = ELEMENT_DATA;
 
   orderform: FormGroup = new FormGroup({
@@ -289,23 +289,16 @@ export class DelorderComponent implements OnInit, AfterViewInit, DataSource {
   }
 }
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface MatTableSummary {
+  hamperCode: string;
+  eyeselInvQty: number;
+  delNotesQty: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: MatTableSummary[] = [
+  {hamperCode: 'Hydrogen', eyeselInvQty: 1.0079, delNotesQty: 2},
+  {hamperCode: 'Helium', eyeselInvQty: 4.0026, delNotesQty: 1},
+  {hamperCode: 'Lithium', eyeselInvQty: 6.941, delNotesQty: 3},
+  {hamperCode: 'Beryllium', eyeselInvQty: 9.0122, delNotesQty: 5},
 ];
 
